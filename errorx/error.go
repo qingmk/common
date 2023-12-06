269a42fd5a5ea6f1caf8f382b9fb2567afb2362f
qinghua <15070102782@139.com>
2023-12-06 11:37:55 +0800

    init

---
+++ errorx/error.go
@@ -0,0 +1,38 @@
+package errorx
+
+const ErrorCode = 500
+const SuccessCode = 200
+const SuccessMessage = "success"
+
+type CodeError struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+type CodeErrorResponse struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+func NewCodeError(code int, msg string) error {
+	return &CodeError{Code: code, Message: msg}
+}
+
+func NewDefaultError(msg string) error {
+	return NewCodeError(ErrorCode, msg)
+}
+
+func NewSuccessTip() error {
+	return NewCodeError(SuccessCode, SuccessMessage)
+}
+
+func (e *CodeError) Error() string {
+	return e.Message
+}
+
+func (e *CodeError) Data() *CodeErrorResponse {
+	return &CodeErrorResponse{
+		Code:    e.Code,
+		Message: e.Message,
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module gitee.com/qingmk/common
+
+go 1.21.3
