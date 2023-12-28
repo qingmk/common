a16222ebd07928beffe04f0cdf44dcf007d61ec3
qinghua <15070102782@139.com>
2023-12-28 08:44:58 +0800

    删除的方法

--- utils/utils.go
+++ utils/utils.go
@@ -13,3 +13,15 @@ func ContainsGeneric[T comparable](slice []T, element T) bool {
 func IsAdmin(roleIdList []int64, roleId int64) bool {
 	return ContainsGeneric(roleIdList, roleId)
 }
+
+// DeleteSlice2 删除指定元素。
+func DeleteSlice2[T comparable](a []T, elem T) []T {
+	//tmp := make([]T, len(a))
+	var tmp []T
+	for _, v := range a {
+		if v != elem {
+			tmp = append(tmp, v)
+		}
+	}
+	return tmp
+}
