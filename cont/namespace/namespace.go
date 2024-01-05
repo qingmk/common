a686f9ad31bcf3ad3666644e60ad6315e86a5767
qinghua <15070102782@139.com>
2024-01-05 10:43:50 +0800

    命名空间三方类型id

---
+++ cont/namespace/namespace.go
@@ -0,0 +1,14 @@
+package namespace
+
+const (
+
+	//命名空间关联类型中的角色
+	TYPEROLE int64 = 1
+	//命名空间关联类型中的用户
+	TYPEUSER int64 = 2
+
+	//命名空间关联类型中的资源类型
+	TYPERESOURCETYPE = 3
+	//默认的命名空间
+	DEFAULTNAMESPACEID int64 = 10
+)
