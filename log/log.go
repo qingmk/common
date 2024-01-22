8ab335776b70e7c945804a6ee7ea659a377927b5
qinghua <15070102782@139.com>
2024-01-22 08:43:29 +0800

    日志更新

--- log/log.go
+++ log/log.go
@@ -2,7 +2,9 @@ package log
 
 import (
 	"encoding/json"
+	"time"
 
+	"github.com/qingmk/common/cont/log"
 	"github.com/qingmk/common/dto"
 	service "github.com/qingmk/common/server"
 )
@@ -13,6 +15,8 @@ type Logger struct {
 	// 定义一个chan
 	MsgChan chan dto.LogMsg
 	Topic   string
+	Node    string //节点名称
+	Service string //那个服务发送的信息
 }
 
 // 这里是不是还应该传kafka的集群地址
@@ -24,6 +28,19 @@ func NewLogger(Brokers []string, Topic string) *Logger {
 		KafkaService: KafkaService,
 		MsgChan:      make(chan dto.LogMsg, 1000),
 		Topic:        Topic,
+	}
+}
+
+func NewLoggerV2(Brokers []string, Topic string, Node string, Service string) *Logger {
+
+	KafkaService := service.NewKafkaServiceV2(Brokers)
+
+	return &Logger{
+		KafkaService: KafkaService,
+		MsgChan:      make(chan dto.LogMsg, 1000),
+		Topic:        Topic,
+		Node:         Node,
+		Service:      Service,
 	}
 }
 
@@ -50,5 +67,41 @@ func (logger *Logger) ReadLog() (err error) {
 		logger.KafkaService.SendKafkaMessage(logger.Topic, string(bytes))
 	}
 
+	return
+}
+
+func (logger *Logger) Error(mgs string) (err error) {
+	message := dto.LogMsg{
+		Service: logger.Service,
+		Msg:     mgs,
+		Level:   log.ERROR,
+		Node:    logger.Node,
+		Time:    time.Now().Format("2006-01-02 15:04:05"),
+	}
+	logger.MsgChan <- message
+	return
+}
+
+func (logger *Logger) Warn(mgs string) (err error) {
+	message := dto.LogMsg{
+		Service: logger.Service,
+		Msg:     mgs,
+		Level:   log.WARN,
+		Node:    logger.Node,
+		Time:    time.Now().Format("2006-01-02 15:04:05"),
+	}
+	logger.MsgChan <- message
+	return
+}
+
+func (logger *Logger) INFO(mgs string) (err error) {
+	message := dto.LogMsg{
+		Service: logger.Service,
+		Msg:     mgs,
+		Level:   log.WARN,
+		Node:    logger.Node,
+		Time:    time.Now().Format("2006-01-02 15:04:05"),
+	}
+	logger.MsgChan <- message
 	return
 }
