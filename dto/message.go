c615c52ab9a535be432e2999044edb19c8c4f9bd
qinghua <15070102782@139.com>
2024-01-29 09:29:08 +0800

    新增namespaceId

--- dto/message.go
+++ dto/message.go
@@ -1,9 +1,10 @@
 package dto
 
 type LogMsg struct {
-	Node    string //节点名称
-	Time    string //日志时间
-	Msg     string //日志信息
-	Service string //那个服务发送的信息
-	Level   string //日志级别
+	Node        string //节点名称
+	Time        string //日志时间
+	Msg         string //日志信息
+	Service     string //那个服务发送的信息
+	Level       string //日志级别
+	NameSpaceId int64  //命名空间id
 }
